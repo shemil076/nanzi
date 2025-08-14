e06ca81605321448aae211b6c1d502521567b2cd
shemil076 <shemil076@gmail.com>
2025-08-14 18:59:40 +0530

    Implemented the ui of the registration for invited tenants

--- src/app/(auth)/tenant-accept-invitation/components/registration-component.tsx
+++ src/app/(auth)/tenant-accept-invitation/components/registration-component.tsx
@@ -1,29 +1,18 @@
 'use client';
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from '@/components/ui/card';
-import { Separator } from '@/components/ui/separator';
-import { Building2, User } from 'lucide-react';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
 import z from 'zod';
+import { Card, CardContent } from '../../../../components/ui/card';
+import { useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormLabel,
-} from '@/components/ui/form';
+} from '../../../../components/ui/form';
 import { Input } from '@/components/ui/input';
-import { RegisterCredentials } from '@/types/auth';
-import { Role } from '@/types/user';
-import { useRouter } from 'next/navigation';
-import { useAuth } from '../../../../hooks/useAuth';
+import { Button } from '../../../../components/ui/button';
 
 const formSchema = z.object({
   email: z.string().email('Invalid email format'),
@@ -34,10 +23,8 @@ const formSchema = z.object({
 
 type registrationFormData = z.infer<typeof formSchema>;
 
-const SignUp = ({ isTenant }: { isTenant: boolean }) => {
+const RegistrationComponent = () => {
   const router = useRouter();
-  const [isTenantSignup, setIsTenantSignUp] = useState(isTenant);
-
   const form = useForm<registrationFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -47,92 +34,28 @@ const SignUp = ({ isTenant }: { isTenant: boolean }) => {
       password: '',
     },
   });
-
-  const { user, register } = useAuth();
-
-  const handleOnSubmit = async (value: z.infer<typeof formSchema>) => {
-    if (value) {
-      const newUser: RegisterCredentials = {
-        firstName: value.firstName,
-        lastName: value.lastName,
-        email: value.email,
-        password: value.password,
-        role: isTenantSignup ? Role.TENANT : Role.LANDLORD,
-      };
-      await register(newUser);
-
-      if (user) {
-        router.push('/dashboard');
-      }
-    }
-  };
-
   return (
-    <Card className={`w-full m-5 sm:w-1/2`}>
-      <CardHeader>
-        <div className="flex flex-col items-center gap-5">
+    <Card className="w-full">
+      <CardContent className="w-full flex flex-col gap-5">
+        <div>
           <div className="museo-font text-3xl text-center">nanzi</div>
 
-          <div className="text-center m-2">
+          <div className="flex flex-col gap-3 text-center m-2">
             <span className="text-3xl font-bold">Create Your Account</span>
             <div>
-              {isTenantSignup ? (
-                <div>
-                  Join <span className="museo-font">nanzi</span> as a Tenant
-                </div>
-              ) : (
-                <div>
-                  Join <span className="museo-font">nanzi</span> as a Landlord
-                </div>
-              )}
+              <div>
+                Join <span className="museo-font">nanzi</span> as a Tenant
+              </div>
             </div>
           </div>
-
-          <div className="w-full flex flex-col sm:flex-row space-x-1">
-            <Button
-              className="sm:w-1/2"
-              variant={isTenantSignup ? 'outline' : 'default'}
-              onClick={() => setIsTenantSignUp(false)}
-            >
-              <Building2 />
-              <span>Landlord</span>
-            </Button>
-            <Button
-              className="sm:w-1/2"
-              variant={isTenantSignup ? 'default' : 'outline'}
-              onClick={() => setIsTenantSignUp(true)}
-            >
-              <User />
-              <span>Tenant</span>
-            </Button>
-          </div>
-          <Separator />
         </div>
-      </CardHeader>
-      <CardContent>
         <Form {...form}>
           <form
             onSubmit={(event) => {
               event.preventDefault();
             }}
           >
-            <div className="flex flex-col gap-2">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="border text-center disabled:text-black disabled:opacity-100"
-                        {...field}
-                        placeholder="ex: tom@mail.com"
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
+            <div className="flex flex-col w-full gap-5">
               <div className="w-full flex flex-row  gap-5">
                 <FormField
                   control={form.control}
@@ -167,6 +90,7 @@ const SignUp = ({ isTenant }: { isTenant: boolean }) => {
                   )}
                 />
               </div>
+
               <FormField
                 control={form.control}
                 name="password"
@@ -185,20 +109,13 @@ const SignUp = ({ isTenant }: { isTenant: boolean }) => {
                 )}
               />
 
-              <Button
-                onClick={() => {
-                  form.handleSubmit(handleOnSubmit)();
-                }}
-              >
-                Create Account
-              </Button>
+              <Button onClick={() => {}}>Create Account</Button>
             </div>
           </form>
         </Form>
       </CardContent>
-      <CardFooter></CardFooter>
     </Card>
   );
 };
 
-export default SignUp;
+export default RegistrationComponent;
--- src/app/(auth)/tenant-accept-invitation/page.tsx
+++ src/app/(auth)/tenant-accept-invitation/page.tsx
@@ -5,11 +5,13 @@ import { useVerifyTenantInvitationToken } from '../../../hooks/use-tenant-invita
 import { useEffect } from 'react';
 import { Card, CardContent } from '../../../components/ui/card';
 import Spinner from '../../../components/custom/spinner';
+import RegistrationComponent from './components/registration-component';
+import { Ban, ClockFading } from 'lucide-react';
 
 const AcceptInvitationPage = () => {
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
-  const { tenantInvitation, verifyTenantInvitation, isLoading } =
+  const { tenantInvitation, verifyTenantInvitation, isLoading, error } =
     useVerifyTenantInvitationToken();
 
   useEffect(() => {
@@ -21,7 +23,7 @@ const AcceptInvitationPage = () => {
 
   if (isLoading) {
     return (
-      <div className="w-full h-screen flex flex-col items-center justify-center">
+      <div className="w-full h-screen flex flex-col items-center justify-center bg-blue-100 ">
         <Card className="w-1/3 h-1/2 flex flex-col items-center justify-center">
           <CardContent className="flex flex-col gap-10">
             <Spinner size={100} color="grey" />
@@ -33,16 +35,32 @@ const AcceptInvitationPage = () => {
       </div>
     );
   }
+
+  if (error) {
+    return (
+      <div className="w-full h-screen flex flex-col items-center justify-center bg-blue-100">
+        <Card className="w-1/3 h-1/2 flex flex-col items-center justify-center">
+          <CardContent className="flex flex-col gap-10 items-center justify-center">
+            <ClockFading size={100} className="animate-pulse" color="grey" />
+            <div>The invitation has expired</div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center">
-      {tenantInvitation && (
-        <div className="flex flex-col">
-          Invitation details
-          <span>id: {tenantInvitation.id}</span>
-          <span>Email: {tenantInvitation.email}</span>
-          <span>property id: {tenantInvitation.propertyId}</span>
-          <span>booking id: {tenantInvitation.bookingId}</span>
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-blue-100">
+      {tenantInvitation ? (
+        <div className="w-1/3 h-1/2 flex flex-col items-center justify-center">
+          <RegistrationComponent />
         </div>
+      ) : (
+        <Card className="w-1/3 h-1/2 flex flex-col items-center justify-center">
+          <CardContent className="flex flex-col gap-10 items-center justify-center">
+            <Ban size={100} className="animate-pulse" color="red" />
+            <div>401 Unauthorized</div>
+          </CardContent>
+        </Card>
       )}
     </div>
   );
